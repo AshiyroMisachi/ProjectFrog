cb05349a544b0d8e988143bda85c18be1201f745
AshiyroMisachi <riallot.alexandre@outlook.fr>
2023-05-23 21:33:19 +0200

    Tiled

---
+++ maps/tileset_placeHolder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset_placeHolder" tilewidth="64" tileheight="64" tilecount="10000" columns="100">
+ <image source="tileset_placeHolder.png" width="6400" height="6400"/>
+</tileset>
